4386e46d208e8897c47e1e24c8af243c7a7bab00
Lukas Olson <olson.lukas@gmail.com>
2016-03-15 10:32:30 -0700

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.1)

---
+++ index.js
@@ -0,0 +1,30 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      app: {
+        title: 'Kibana Es Indexer',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/kibana_es_indexer/app'
+      },
+      hacks: [
+        'plugins/kibana_es_indexer/hack'
+      ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+
+  });
+};
